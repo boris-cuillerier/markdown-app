3381ec645bb414c48b1cb6ef6d60f1ad103fe2d4
Aliocha <aliocha@MacBook-Air-de-Aliocha.local>
2025-08-05 14:42:27 +0200

    Add Bootstrap and change CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, {Component} from "react";
-import "./App.css";
+import React, {Component} from 'react';
+import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <div>PAF!</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea 
+              className='form-control'
+              rows="35">
+                
+            </textarea>
+          </div>
+          <div className="col-sm-6">
+            <h1>Résultat</h1>
+          </div>
+        </div>
+      </div>
     )
   }
 };
