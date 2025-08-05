8c16a62b70000e7a9fb6bf022c6f6107569b5fe5
Aliocha <aliocha@MacBook-Air-de-Aliocha.local>
2025-08-05 23:35:16 +0200

    Implementation of the textarea State

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import { sampleText } from './sampleText.js';
 
 class App extends Component {
+  state = {
+    text: sampleText
+  };
+  handleChange = event => {
+    const text = event.target.value;
+    this.setState({ text });
+  };
   render () {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6">
-            <textarea 
-              value={ sampleText }
+            <textarea
+              onChange={ this.handleChange } 
+              value={ this.state.text }
               className='form-control'
               rows="35">
                 
